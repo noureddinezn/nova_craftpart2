fd3444ed015f728cbbc0a811b28ad6d88303d315
noureddinezn <noureddinezouana@gmail.com>
2025-12-21 21:13:03 +0100

    la creatin de la page about avec h1 et  paragraph

---
+++ views/home.php
@@ -0,0 +1,14 @@
+<section class="container mx-auto py-20 text-center max-w-2xl">
+    <h2 class="text-4xl font-bold mb-6">
+        Propulsez votre entreprise vers le digital
+    </h2>
+
+    <p class="text-lg mb-8 text-gray-600">
+        Création de sites Web, automatisation, solutions digitales sur mesure.
+    </p>
+
+    <a href="/service"
+       class="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700">
+        Voir nos services
+    </a>
+</section>
