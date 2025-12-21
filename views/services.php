05edb5f31838a41f8bc25b9316a7a1d9e19f5e8f
noureddinezn <noureddinezouana@gmail.com>
2025-12-21 22:09:48 +0100

    la creation de la pages serveces et l h1 et paragraph

---
+++ views/services.php
@@ -0,0 +1,12 @@
+<section class="container mx-auto py-16 px-4 md:px-0">
+    <h2 class="text-3xl font-bold mb-8 text-center">Nos Services</h2>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div class="bg-white p-6 shadow-md rounded-lg hover:shadow-lg transition">
+            <h3 class="text-xl font-bold mb-2">Création de sites Web</h3>
+            <p class="text-gray-600">Sites vitrines, e-commerce, applications modernes.</p>
+        </div>
+
+     
+    </div>
+</section>
