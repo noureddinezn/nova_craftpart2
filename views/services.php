f32cd2087811679b7d38f2539f4938090c65278f
noureddinezn <noureddinezouana@gmail.com>
2025-12-21 22:10:20 +0100

    la creation de la pages serveces et le contenu

--- views/services.php
+++ views/services.php
@@ -7,6 +7,14 @@
             <p class="text-gray-600">Sites vitrines, e-commerce, applications modernes.</p>
         </div>
 
-     
+        <div class="bg-white p-6 shadow-md rounded-lg hover:shadow-lg transition">
+            <h3 class="text-xl font-bold mb-2">Développement sur mesure</h3>
+            <p class="text-gray-600">Outils internes, solutions métiers spécifiques.</p>
+        </div>
+
+        <div class="bg-white p-6 shadow-md rounded-lg hover:shadow-lg transition">
+            <h3 class="text-xl font-bold mb-2">Automatisation & API</h3>
+            <p class="text-gray-600">Automatisation des tâches, intégration d'API.</p>
+        </div>
     </div>
 </section>
