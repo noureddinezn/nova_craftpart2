abba4d73df3987647d86acdb12e7ccd635c5d4d9
noureddinezn <noureddinezouana@gmail.com>
2025-12-21 22:20:26 +0100

    la creation  de la page registrer est apres cre le contenu

---
+++ views/registrer.php
@@ -0,0 +1,27 @@
+<div class="min-h-screen flex items-center justify-center">
+  <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+    <h2 class="text-3xl font-bold text-center mb-6 text-blue-600">Inscription</h2>
+
+        <form class="space-y-4" method="POST" action="">
+        <input type="text" name="name" placeholder="Nom complet"
+          class="w-full border px-4 py-2 rounded-lg" required>
+      
+        <input type="email" name="email" placeholder="Email"
+          class="w-full border px-4 py-2 rounded-lg" required>
+      
+        <input type="password" name="password" placeholder="Mot de passe"
+          class="w-full border px-4 py-2 rounded-lg" required>
+      
+        <input type="password" name="confirm_password" placeholder="Confirmer le mot de passe"
+          class="w-full border px-4 py-2 rounded-lg" required>
+      
+        <button
+          class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+          Créer un compte
+        </button>
+      </form>
+
+
+   
+  </div>
+</div>
