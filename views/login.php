81c968990040a8e359d5b765ad3383f214c02a8a
noureddinezn <noureddinezouana@gmail.com>
2025-12-21 22:06:13 +0100

    la creation de page login est la creation de form

---
+++ views/login.php
@@ -0,0 +1,21 @@
+ <form method="POST" class="space-y-4">
+
+      <input type="email" name="email" placeholder="Email"
+        class="w-full border px-4 py-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+        required>
+
+      <input type="password" name="password" placeholder="Mot de passe"
+        class="w-full border px-4 py-2 rounded-lg focus:ring-2 focus:ring-blue-500"
+        required>
+
+      <button
+        class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+        Se connecter
+      </button>
+
+    </form>
+
+    
+
+  </div>
+</div>
